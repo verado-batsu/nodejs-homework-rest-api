863df3a91c44988b40f6a5aae6d508961189a212
Vladyslav Kupchuk <kupchuk.vlad@gmail.com>
2023-05-29 00:03:51 +0300

    FIXED: wrote another messages for some errors response

--- decorators/validateBody.js
+++ decorators/validateBody.js
@@ -2,6 +2,9 @@ const { HttpError } = require("../helpers");
 
 const validateBody = schema => {
 	const func = (req, res, next) => {
+		if (!req.body) {
+			next(HttpError(400, "missing fields"));
+		}
 		const { error } = schema.validate(req.body);
 		if (error) {
 			next(HttpError(400, error.message));
--- schemas/contacts.js
+++ schemas/contacts.js
@@ -2,9 +2,15 @@ const Joi = require('joi');
 
 
 const contactAddSchema = Joi.object({
-	name: Joi.string().required(),
-	email: Joi.string().email().required(),
-	phone: Joi.required()
+	name: Joi.string().required().messages({
+		'any.required': `missing required name field`
+    }),
+	email: Joi.string().email().required().messages({
+		'any.required': `missing required email field`
+    }),
+	phone: Joi.required().messages({
+		'any.required': `missing required phone field`
+    })
 })
 
 module.exports = {
